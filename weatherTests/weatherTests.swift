ce33577ac5270f79f9c83c68e419d0409bce5c7a
Aloy's entourage <entourage@fidelity.com>
2025-02-02 13:00:37 -0500

    init

---
+++ weatherTests/weatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  weatherTests.swift
+//  weatherTests
+//
+//  Created by Entourage on 2025/2/2.
+//
+
+import Testing
+@testable import weather
+
+struct weatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
